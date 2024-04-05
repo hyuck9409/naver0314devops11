3eddc96c948ae647b1f82aa9eae3e74c0c114922
hyuck9409 <djfh611@naver.com>
2024-04-05 17:32:55 +0900

    0405

--- javawork/java_tutorials/src/org/opentutorials/javatutorials/numberstring/CharString.java
+++
@@ -1,9 +0,0 @@
-package org.opentutorials.javatutorials.numberstring;
-
-public class CharString {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("1"+"1");
-}
-}
--- javawork/java_tutorials/src/org/opentutorials/javatutorials/numberstring/Number.java
+++
@@ -1,9 +0,0 @@
-package org.opentutorials.javatutorials.numberstring;
-
-public class Number {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println(1.2+2.3);
-}
-}
